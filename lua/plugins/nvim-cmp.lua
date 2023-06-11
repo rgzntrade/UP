4d949499320c21f46b94dda5369a2cd8a45fb466
rgzntrade <309618485@qq.com>
2023-06-11 22:46:18 +0800

    修改cmp的快捷键

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,9 +4,12 @@ return {
 
     dependencies = {
       {
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-cmdline",
+      },
+      {
         "tzachar/cmp-tabnine",
         build = "./install.sh",
-        event = "InsertEnter",
         config = function()
           local tabnine = require("cmp_tabnine.config")
           tabnine:setup({}) -- put your options here
@@ -25,11 +28,53 @@ return {
     },
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.sources =
-        cmp.config.sources(vim.list_extend(opts.sources, { { name = "codeium" }, { name = "cmp-tabnine" } }))
+      opts.sources = cmp.config.sources(
+        vim.list_extend(
+          opts.sources,
+          { { name = "codeium" }, { name = "cmdline" }, { name = "nvim_lua" }, { name = "cmp_tabnine" } }
+        )
+      )
+      opts.mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }),
+
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+      }
     end,
-  },
-  {
-    "stevearc/dressing.nvim",
   },
 }
