81aaf14be5812ef7bfa0c6602e476ec2f6d84d1c
rgzntrade <309618485@qq.com>
2023-07-31 08:38:37 +0800

    添加cmake-tools

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
