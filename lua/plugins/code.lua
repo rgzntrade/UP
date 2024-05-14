565be4515863a4e362022f1e93cd98b9fd5616a9
rgzntrade <309618485@qq.com>
2024-05-14 10:52:46 +0800

    feat: 添加leetcode插件

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -9,4 +9,32 @@ return {
       })
     end,
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+      ---@type string
+      arg = "leetcode.nvim",
+
+      ---@type lc.lang
+      lang = "cpp",
+
+      cn = { -- leetcode.cn
+        enabled = true, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
+      },
+    },
+  },
 }
