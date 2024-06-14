a4a45c9608ff7052811e632f9bdcc116993f5f64
rgzntrade <309618485@qq.com>
2024-06-14 14:17:37 +0800

    fix: 解决fzf中的按键覆盖文件

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,24 +5,31 @@ return {
     optional = true,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-      winopts = {
-        -- split = "belowright new",
-        fullscreen = true, -- start fullscreen?
-        preview = {
-          vertical = "up:60%", -- up|down|left|right|top|bottom
-          horizontal = "right:60%", -- up|down|left|right|top|bottom
-          layout = "vertical", -- horizontal|vertical
+      -- 放在defaults中可以解决一些冲突问题
+      defaults = {
+        winopts = {
+          -- split = "belowright new",
+          fullscreen = true, -- start fullscreen?
+          preview = {
+            vertical = "up:60%", -- up|down|left|right|top|bottom
+            horizontal = "right:60%", -- up|down|left|right|top|bottom
+            layout = "vertical", -- horizontal|vertical
+          },
         },
-      },
-      keymap = {
-        fzf = {
-          ["down"] = "next-history", -- 下一个历史记录
-          ["up"] = "previous-history", -- 上一个历史记录
+        keymap = {
+          fzf = {
+            ["down"] = "next-history", -- 下一个历史记录
+            ["up"] = "previous-history", -- 上一个历史记录
+            ["ctrl-n"] = "down", -- 绑定 ctrl-n 到移动到下一个结果
+            ["ctrl-p"] = "up", -- 绑定 ctrl-p 到移动到上一个结果
+          },
         },
       },
       git = {
-        fzf_opts = {
-          ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-git-files-history",
+        files = {
+          fzf_opts = {
+            ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-git-files-history",
+          },
         },
       },
       oldfiles = {
