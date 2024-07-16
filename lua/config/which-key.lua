502749d05532b3afc017f79d921dc8abc5df6c7a
rgzntrade <309618485@qq.com>
2024-07-16 10:04:44 +0800

    feat: 更新which-key配置

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -4,32 +4,30 @@ if not status_ok then
 end
 
 local opts = {
-  mode = "n", -- NORMAL mode
-  -- prefix: use "<leader>f" for example for mapping everything related to finding files
-  -- the prefix is prepended to every mapping part of `mappings`
-  prefix = "<Space>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
+  -- mode = "n", -- NORMAL mode
+  -- -- prefix: use "<leader>f" for example for mapping everything related to finding files
+  -- -- the prefix is prepended to every mapping part of `mappings`
+  -- prefix = "<Space>",
+  -- buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  -- silent = true, -- use `silent` when creating keymaps
+  -- noremap = true, -- use `noremap` when creating keymaps
+  -- nowait = true, -- use `nowait` when creating keymaps
 }
 
 local mappings = {
-  a = { a = { "<cmd>Dashboard<cr>", "Welcome" } },
-  m = {
-    name = "CMake",
-    g = { "<cmd>CMake configure<CR>", "Configure" },
-    t = { "<cmd>CMake select_target<CR>", "SelectTarget" },
-    T = { "<cmd>CMake select_build_type<CR>", "SelectBuildType" },
-    b = { "<cmd>CMake build<CR>", "BuildTarget" },
-    a = { "<cmd>CMake build_all<CR>", "BuildAll" },
-    r = { "<cmd>CMake build_and_run<CR>", "Run" },
-    d = { "<cmd>CMake build_and_debug<CR>", "DebugTarget" },
-    q = { "<cmd>CMake cancel<CR>", "Cancel" },
-    s = { "<cmd>CMake set_target_args<CR>", "SetArg" },
-    c = { "<cmd>CMake clear_cache<CR>", "SetArg" },
-    -- TODO: 增加 +code
-  },
+
+  { "<Space>aa", "<cmd>Dashboard<cr>", desc = "Welcome", nowait = true, remap = false },
+  { "<Space>m", group = "CMake", nowait = true, remap = false },
+  { "<Space>mT", "<cmd>CMake select_build_type<CR>", desc = "SelectBuildType", nowait = true, remap = false },
+  { "<Space>ma", "<cmd>CMake build_all<CR>", desc = "BuildAll", nowait = true, remap = false },
+  { "<Space>mb", "<cmd>CMake build<CR>", desc = "BuildTarget", nowait = true, remap = false },
+  { "<Space>mc", "<cmd>CMake clear_cache<CR>", desc = "SetArg", nowait = true, remap = false },
+  { "<Space>md", "<cmd>CMake build_and_debug<CR>", desc = "DebugTarget", nowait = true, remap = false },
+  { "<Space>mg", "<cmd>CMake configure<CR>", desc = "Configure", nowait = true, remap = false },
+  { "<Space>mq", "<cmd>CMake cancel<CR>", desc = "Cancel", nowait = true, remap = false },
+  { "<Space>mr", "<cmd>CMake build_and_run<CR>", desc = "Run", nowait = true, remap = false },
+  { "<Space>ms", "<cmd>CMake set_target_args<CR>", desc = "SetArg", nowait = true, remap = false },
+  { "<Space>mt", "<cmd>CMake select_target<CR>", desc = "SelectTarget", nowait = true, remap = false },
 }
 
-which_key.register(mappings, opts)
+which_key.add(mappings)
