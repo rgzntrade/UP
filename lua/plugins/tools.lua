7d8de03fa51194811fbb532e025d3a81663c647d
rgzntrade <309618485@qq.com>
2024-09-24 13:39:27 +0800

    feat: 增加pomodoro插件

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -83,4 +83,14 @@ return {
   --     require("render-markdown").setup({})
   --   end,
   -- },
+  {
+    "quentingruber/pomodoro.nvim",
+    lazy = false, -- needed so the pomodoro can start at launch
+    opts = {
+      start_at_launch = true,
+      work_duration = 25,
+      break_duration = 5,
+      snooze_duration = 1, -- The additionnal work time you get when you delay a break
+    },
+  },
 }
