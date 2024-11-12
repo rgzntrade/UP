273df14fd38110a6bf0a2975a5d086ea26f9dbc6
rgzntrade <309618485@qq.com>
2024-11-12 16:41:59 +0800

    feat: 优化窗口显示

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,6 +5,15 @@ return {
     opts = {
       --[[ things you want to change go here]]
       shade_terminals = false,
+      float_opts = {
+        border = 'curved',
+      },
+      -- winbar = {
+      --   enabled = true,
+      --   name_formatter = function(term) --  term: Terminal
+      --     return term.name
+      --   end,
+      -- },
     },
   },
 }
