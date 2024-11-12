f53936a5ef8400641282862510531376a636bbb8
rgzntrade <309618485@qq.com>
2024-11-12 14:29:11 +0800

    fix: 同步更新配置

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -3,11 +3,12 @@ return {
     "stevearc/conform.nvim",
     opts = {
       -- LazyVim will use these options when formatting with the conform.nvim formatter
-      format = {
+      default_format_opts = {
         timeout_ms = 1000,
       },
       formatters_by_ft = {
         cpp = { "clang-format" },
+        cmake = { "cmakelang" },
       },
       formatters = {
         clang_format = { prepend_args = { "--style=file" } },
