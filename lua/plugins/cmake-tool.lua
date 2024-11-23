045105149052e85a0b4edfc6c333e4ad1f3b20ab
rgzntrade <309618485@qq.com>
2024-11-23 16:38:41 +0800

    feat: 用cmake-tool替换nvim-cmake

---
+++ lua/plugins/cmake-tool.lua
@@ -0,0 +1,18 @@
+local utils = require("config.utils")
+local cmake_and_cpp_file_types = utils.array.union(utils.file_types.cmake_file_types, utils.file_types.cpp_file_types)
+
+return {
+  {
+    "Civitasv/cmake-tools.nvim",
+    ft = cmake_and_cpp_file_types,
+    opts = function(_, opts)
+      local cmake = require("cmake-tools")
+      opts.cmake_build_directory = function()
+        if vim.fn.has("win32") then
+          return "build\\${variant:buildType}"
+        end
+        return "build/${variant:buildType}"
+      end
+    end,
+  },
+}
