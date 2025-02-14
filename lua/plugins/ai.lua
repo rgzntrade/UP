029ed3d215965ba8dee4cdd640a40a784b994fe9
rgzntrade <309618485@qq.com>
2025-02-14 17:32:34 +0800

    feat: 移除一个第三方的ai

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,51 +1,51 @@
 return {
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    opts = {
-      -- add any opts here
-      provider = "gemini",
-    },
-    keys = {
-      {
-        "<leader>aa",
-        function()
-          require("avante.api").ask()
-        end,
-        desc = "avante: ask",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>ar",
-        function()
-          require("avante.api").refresh()
-        end,
-        desc = "avante: refresh",
-      },
-      {
-        "<leader>ab",
-        function()
-          require("avante.api").edit()
-        end,
-        desc = "avante: edit",
-        mode = "v",
-      },
-    },
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      {
-        -- Make sure to setup it properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   opts = {
+  --     -- add any opts here
+  --     provider = "gemini",
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>aa",
+  --       function()
+  --         require("avante.api").ask()
+  --       end,
+  --       desc = "avante: ask",
+  --       mode = { "n", "v" },
+  --     },
+  --     {
+  --       "<leader>ar",
+  --       function()
+  --         require("avante.api").refresh()
+  --       end,
+  --       desc = "avante: refresh",
+  --     },
+  --     {
+  --       "<leader>ab",
+  --       function()
+  --         require("avante.api").edit()
+  --       end,
+  --       desc = "avante: edit",
+  --       mode = "v",
+  --     },
+  --   },
+  --   dependencies = {
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     {
+  --       -- Make sure to setup it properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
 }
