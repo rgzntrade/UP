9a37ce02fa503743cea31cd2e61323695f60d576
rgzntrade <309618485@qq.com>
2025-03-05 15:48:36 +0800

    feat: 增加neovide配置

---
+++ lua/config/neovide.lua
@@ -0,0 +1,10 @@
+if vim.g.neovide then
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_fullscreen = true
+
+  vim.keymap.set("v", "<C-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<C-S-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<C-S-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<C-S-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<C-S-v>", "<C-R>+") -- Paste insert mode
+end
